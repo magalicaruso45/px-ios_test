e556ccbe16b187bdb27025b5d04129c90ea64e98
Demian Tejo <demian.tejo+github@gmail.com>
2018-07-11 15:38:41 -0300

    Add Comisiones Test

---
+++ MercadoPagoSDKExampleSwiftUITests/Test Flow/ChargesTest.swift
@@ -0,0 +1,44 @@
+//
+//  ChargesTest.swift
+//  MercadoPagoSDKExampleSwiftUITests
+//
+//  Created by Demian Tejo on 11/7/18.
+//  Copyright © 2018 Mercado Pago. All rights reserved.
+//
+
+import XCTest
+
+class ChargesTest: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func test_flujo_con_comision_para_creditcard(){
+        MainScreen()
+            .tapConfigurationsButton()
+            .changeComisionesSwitch()
+            .tapApplyConfigurationsButton()
+            .tapClearButton()
+            .fillPublicKey("APP_USR-648a260d-6fd9-4ad7-9284-90f22262c18d")
+            .fillPreferenceId("243966003-d0be0be0-6fd8-4769-bf2f-7f2d979655f5")
+            .tapCheckoutOption()
+            .tapCardOption()
+            .tapCreditCardOption()
+            .completeNumberAndContinue("5323 7937 3550 6106")
+            .completeNameAndContinue("APRO")
+            .completeExpirationDateAndContinue("1225")
+            .completeCVVAndContinue("123")
+            .completeNumberAndContinueToIssuers("30666777")
+            .selectIssuerOptionToPayerCostScreenAtRow(1)
+            .selectPayerCostOptionAtRow(2)
+            .tapPayButtonForAnyCongrats()
+    }
+    
+}
