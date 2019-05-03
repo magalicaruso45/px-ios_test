835df7f4ca699868e70e34518a937cd0858733d0
Federico Bustos Fierro <federico.bustos@mercadolibre.com>
2019-05-03 13:30:51 -0300

    move presets to plists

--- MercadoPagoSDKExampleSwiftUITests/Test Flow/OneTapTests.swift
+++ MercadoPagoSDKExampleSwiftUITests/Test Flow/OneTapTests.swift
@@ -29,11 +29,8 @@ class OneTapTests: XCTestCase {
             .changeAccessTokenSwitch()
             .changeAdvancedSwitch()
             .changeOneTapSwitch()
+            .tapOtherPreferenceSegment(.oneTap)
             .tapApplyConfigurationsButton()
-            .tapClearButton()
-            .fillAccessToken("TEST-1458038826212807-062020-ff9273c67bc567320eae1a07d1c2d5b5-246046416")
-            .fillPublicKey("TEST-4763b824-93d7-4ca2-a7f7-93539c3ee5bd")
-            .fillPreferenceId("242624092-2a26fccd-14dd-4456-9161-5f2c44532f1d")
             .tapCheckoutOptionForOneTap()
     }
 
